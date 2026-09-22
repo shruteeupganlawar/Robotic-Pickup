a4dea43cf899fb1f78b0f517e7b3dafdebb39647
Shrutee <upg06shrutee@gmail.com>
2026-09-22 18:42:51 +0530

    Scene setup

--- src/scene_setup.py
+++ src/scene_setup.py
@@ -1,29 +1,32 @@
-#basic scene setup
 import pybullet as p
 import pybullet_data
 import numpy as np
+
 
 def setup_scene(gui=True):
     p.connect(p.GUI if gui else p.DIRECT)
     p.setAdditionalSearchPath(pybullet_data.getDataPath())
     p.setGravity(0, 0, -9.81)
 
-    plane_id = p.loadURDF("plane.urdf")
-    robot_id = p.loadURDF("franka_panda/panda.urdf", useFixedBase=True, basePosition=[0, 0, 0])
+    plane_id = p.loadURDF("plane.urdf", basePosition=[0, 0, -0.65])
     table_id = p.loadURDF("table/table.urdf", basePosition=[0.5, 0, -0.65])
-    cube_id  = p.loadURDF("cube_small.urdf", basePosition=[0.5, 0.1, 0.05])
-    duck_id  = p.loadURDF("duck_vhacd.URDF", basePosition=[0.45, -0.1, 0.05], globalScaling=1.2)
+    robot_id = p.loadURDF("franka_panda/panda.urdf", useFixedBase=True,
+                          basePosition=[0, 0, 0])
 
-    return{
-        "robot" : robot_id,
-        "objects" : {"cube": cube_id, "duck": duck_id},
-        "table" : table_id
+    cube_id = p.loadURDF("cube_small.urdf", basePosition=[0.5, 0.1, 0.0])
+    duck_id = p.loadURDF("duck_vhacd.urdf", basePosition=[0.45, -0.1, 0.0],
+                         globalScaling=1.2)
+
+    return {
+        "robot": robot_id,
+        "objects": {"cube": cube_id, "duck": duck_id},
+        "table": table_id,
     }
 
+
 if __name__ == "__main__":
-    scene = setup_scene(gui=True)
     import time
+    scene = setup_scene(gui=True)
     for _ in range(2000):
         p.stepSimulation()
-        time.sleep(1/240)
-
+        time.sleep(1 / 240)
